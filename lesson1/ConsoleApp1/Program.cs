f0dcfad122c56f3b0006c1040443852c29c6dfc2
Elizaveta Kuznetsova <lizka021101@yandex.ru>
2023-03-12 22:25:07 +0300

    add homework for lesson1

---
+++ lesson1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main ()
+    {
+        Console.WriteLine("Введите свое имя");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Привет, " + Name + "!");
+    }
+}
